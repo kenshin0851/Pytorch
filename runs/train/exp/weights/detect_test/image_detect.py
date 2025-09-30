bc2670609eb79ce04f36996080419c0cc6259b84
kimken0424 <kenshin0851@gmail.com>
2025-09-30 18:38:18 +0900

    Update image_detect.py

--- runs/train/exp/weights/detect_test/image_detect.py
+++ runs/train/exp/weights/detect_test/image_detect.py
@@ -11,6 +11,15 @@ import os
 import torch
 from ultralytics import YOLO
 
+import platform
+import pathlib
+
+# Check the operating system and set the appropriate path type
+if platform.system() == 'Windows':
+    pathlib.PosixPath = pathlib.WindowsPath
+else:
+    pathlib.WindowsPath = pathlib.PosixPath
+
 # 1. 학습된 모델 불러오기
 # 학습 완료 후 생성된 'best.pt' 파일을 로드합니다.
 # 이 파일은 학습 과정에서 가장 좋은 성능을 보인 모델의 가중치입니다.
@@ -32,5 +41,6 @@ model.to(device)
 IMAGE_PATH = 'potholes9_png.rf.fe92e8fa27cd17ea446b44207726a8da.jpg'
 
 results = model(IMAGE_PATH)
+
 
 results.show()
